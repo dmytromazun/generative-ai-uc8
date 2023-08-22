12caa31f366c2a34e2c5dcc8e265f7db540a430e
Dmytro Mazun <dm220m@gmail.com>
2023-08-22 17:16:12 +0200

    UC#8 Setup the form

--- app/src/App.js
+++ app/src/App.js
@@ -1,10 +1,13 @@
 import { Provider } from 'react-redux'
 import store from './store'
 import './App.css'
+import { Form } from './components/Form'
 
 const App = () => (
   <Provider store={store}>
-    <div className="App">App</div>
+    <div className="App">
+      <Form />
+    </div>
   </Provider>
 )
 
---
+++ app/src/components/Form.js
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import validator from 'validator'
+import { updateField } from '../store'
+
+export const Form = () => {
+  const dispatch = useDispatch()
+
+  const [formData, setFormData] = useState({
+    first_name: '',
+    last_name: '',
+    email: '',
+    message: '',
+  })
+
+  const [formErrors, setFormErrors] = useState({})
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }))
+  }
+
+  const isFormValid = () => {
+    if (!formData.first_name) return false
+    if (!formData.last_name) return false
+    if (!validator.isEmail(formData.email)) return false
+    if (!formData.message || formData.message.length < 10) return false
+
+    return true
+  }
+
+  const validateFormAndSetErrors = () => {
+    let errors = {}
+
+    if (!formData.first_name) errors.first_name = 'First Name is required'
+    if (!formData.last_name) errors.last_name = 'Last Name is required'
+    if (!validator.isEmail(formData.email)) errors.email = 'Invalid Email'
+    if (!formData.message) {
+      errors.message = 'Message is required'
+    } else if (formData.message.length < 10) {
+      errors.message = 'Message should be at least 10 characters long'
+    }
+
+    setFormErrors(errors)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    validateFormAndSetErrors()
+
+    if (isFormValid()) {
+      for (let field in formData) {
+        dispatch(updateField({ fieldName: field, value: formData[field] }))
+      }
+      console.log('Data submitted to store')
+      setFormData({
+        first_name: '',
+        last_name: '',
+        email: '',
+        message: '',
+      })
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First Name:</label>
+        <input
+          type="text"
+          name="first_name"
+          value={formData.first_name}
+          onChange={handleInputChange}
+        />
+        {formErrors.first_name && <p>{formErrors.first_name}</p>}
+      </div>
+      <div>
+        <label>Last Name:</label>
+        <input
+          type="text"
+          name="last_name"
+          value={formData.last_name}
+          onChange={handleInputChange}
+        />
+        {formErrors.last_name && <p>{formErrors.last_name}</p>}
+      </div>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+        />
+        {formErrors.email && <p>{formErrors.email}</p>}
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleInputChange}
+        />
+        {formErrors.message && <p>{formErrors.message}</p>}
+      </div>
+      <button type="submit" disabled={!isFormValid()}>
+        Submit
+      </button>
+    </form>
+  )
+}
